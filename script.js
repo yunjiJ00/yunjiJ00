b3ced003066f5a75e87efaa640e3fcf340bf9553
yunJ00 <heather2000@naver.com>
2024-05-09 15:04:29 +0900

    Update : Skill fixed

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 function scrollToSection(sectionId) {
-    const section = document.getElementById(sectionId);
-    section.scrollIntoView({ behavior: 'smooth' });
-  }
+  const section = document.getElementById(sectionId);
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  "use strict";
+  var bars = document.querySelectorAll(".bar");
+
+  var methods = {
+    init: function() {
+      methods.bindEvents();
+    },
+    bindEvents: function() {
+      bars.forEach(function(bar) {
+        var pct = bar.querySelector(".pct");
+        var data = JSON.parse(bar.getAttribute("data-bar"));
+
+        setTimeout(function() {
+          bar.style.backgroundColor = data.color;
+          bar.style.width = pct.innerHTML;
+          pct.style.color = data.color;
+          pct.style.opacity = 1;
+        }, data.delay || 0);
+      });
+    }
+  };
+  methods.init();
+});
